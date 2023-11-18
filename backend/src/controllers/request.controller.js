c336172f73a8dac769045e20173da0c3ad78485a
Devchrisar <christopher.ariasar@amigo.edu.co>
2023-11-18 18:01:08 -0500

    feat(backend): Added new controller to handle requests

---
+++ backend/src/controllers/request.controller.js
@@ -0,0 +1,83 @@
+import axios from "axios";
+import RequestModel from "../models/audit.model.js";
+
+//? Controlador para obtener todas las publicaciones (posts)
+async function getPosts(req, res, next) {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    const posts = response.data;
+    res.json(posts);
+  } catch (error) {
+    next(error);
+  }
+}
+
+//? Controlador para obtener álbumes de un usuario específico
+async function getAlbumsByUserId(req, res, next) {
+  const userId = req.query.userId;
+
+  try {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/albums?userId=${userId}`
+    );
+    const albums = response.data;
+    res.json(albums);
+  } catch (error) {
+    next(error);
+  }
+}
+
+//? Controlador para obtener todos los registros de peticiones
+async function getAllRequests(req, res, next) {
+  try {
+    const requests = await RequestModel.find();
+    res.json(requests);
+  } catch (error) {
+    next(error);
+  }
+}
+
+//? Controlador para crear una nueva petición
+async function createRequest(req, res, next) {
+  try {
+    const newRequest = await RequestModel.create(req.body);
+    res.json(newRequest);
+  } catch (error) {
+    next(error);
+  }
+}
+
+//? Controlador para editar un registro de petición
+async function editRequest(req, res, next) {
+  try {
+    const updatedRequest = await RequestModel.findByIdAndUpdate(
+      req.params.requestId,
+      req.body,
+      { new: true }
+    );
+    res.json(updatedRequest);
+  } catch (error) {
+    next(error);
+  }
+}
+
+//? Controlador para eliminar un registro de petición
+async function deleteRequest(req, res, next) {
+  try {
+    await RequestModel.findByIdAndDelete(req.params.requestId);
+    res.json({ message: "Registro eliminado con éxito." });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export {
+  getPosts,
+  getAlbumsByUserId,
+  getAllRequests,
+  createRequest,
+  editRequest,
+  deleteRequest,
+};
--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import getAllUsers from "../controllers/users.controller.js";
-/*import {
+import {
   getPosts,
   getAlbumsByUserId,
   getAllRequests,
   createRequest,
   editRequest,
   deleteRequest,
-} from "../controllers/request.controller";
-*/
+} from "../controllers/request.controller.js";
+
 const router = Router();
 
 // ?Ruta de inicio
@@ -16,28 +16,18 @@ router.route("/api/").get((req, res) => {
   res.render("index", { title: "+Alcance API" });
 });
 
-// ?Rutas para usuarios
+// ?Ruta para leer (GET) usuarios
 router.route("/api/users").get(getAllUsers);
 
-// ?Rutas para publicaciones (posts)
-/*router
-      .route("/api/posts")
-        .get(getPosts)
+// ?Ruta para leer (GET)  publicaciones (posts)
+router.route("/api/posts").get(getPosts)
 
-// ?Rutas para álbumes de un usuario específico
-/*router
-        .route("/api/albums/:userId")
-          .get(getAlbumsByUserId)
+// ?Ruta para leer (GET) álbumes de un usuario específico
+router.route("/api/albums/").get(getAlbumsByUserId)
 
-// ?Rutas para registros de peticiones
-/*router
-        .route("/api/requests")
-          .get(getAllRequests)
-          .post(createRequest)
+// ?Ruta para crear (GET, POST) el registros de la peticion
+router.route("/api/requests").get(getAllRequests).post(createRequest)
+// ?Ruta para editar y eliminar (PATCH/PUT, DELETE) el registros de la peticion
+router.route("/api/requests/:requestId").patch(editRequest).delete(deleteRequest)
 
-/*router
-        .route("/api/requests/:requestId")
-          .patch(editRequest)
-          .delete(deleteRequest)
-*/
 export default router;
